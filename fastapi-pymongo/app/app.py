ab0fc365463cdfd664b00ed81d844fc7779dfbba
Ananya Karra <66985689+anyakara@users.noreply.github.com>
2024-01-06 21:40:04 -0800

    Update app.py

--- fastapi-pymongo/app/app.py
+++ fastapi-pymongo/app/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 app.include_router(router)
 
 from pymongo.mongo_client import MongoClient
-uri = "mongodb+srv://annykarra:monkey1234@cluster0.cump9sk.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://<username>:<password>@cluster0.cump9sk.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri) #, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
